1aaf4099360b576fb0c634dd7d44f7fc4378805f
Sangram <sangram15.behera@gmail.com>
2022-05-21 02:27:38 +0530

    Added By Sangram

---
+++ Data-Structures-and-Algorithms/Program.cs
@@ -0,0 +1,28 @@
+﻿using  DSAlgorithmProgram;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To Algorithm Programs");
+        bool end = true;
+        Console.WriteLine("1.BinarySearch\n2.End the Program");
+        while (end == true)
+        {
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearch binarySearch = new BinarySearch();
+                    binarySearch.Binary();
+                    break;
+                case 2:
+                    end = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter Proper Option To Execute");
+                    break;
+            }
+        }
+    }
+}
