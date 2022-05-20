636439fdc574825c48882c213295d361efd9bec4
Sangram <sangram15.behera@gmail.com>
2022-05-21 02:26:54 +0530

    Added By Sangram

---
+++ Data-Structures-and-Algorithms/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DSAlgorithmProgram
+{
+    public class BinarySearch
+    {
+        public void Binary()
+        {
+            bool check = false;
+            const string Data_filepath = @"C:\Repository\Data-Structures-and-Algorithms\Data-Structures-and-Algorithms\Data.txt";
+            string list = File.ReadAllText(Data_filepath);
+            string[] arr = list.Split(" ");
+            Console.WriteLine("Enter The Word");
+            string name = Console.ReadLine();
+            foreach (string word in arr)
+            {
+                if (word.Equals(name))
+                {
+                    check = true;
+                    Console.WriteLine("Word Is Available");
+                }
+
+            }
+            if (check == false)
+            {
+                Console.WriteLine("Word Is not Available");
+            }
+
+        }
+    }
+}
