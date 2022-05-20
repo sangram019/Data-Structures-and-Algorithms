1107f3a5e36df875b1f297b5cb1715f8805a46c5
Sangram <sangram15.behera@gmail.com>
2022-05-21 02:39:44 +0530

    Added By Sangram

---
+++ Data-Structures-and-Algorithms/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgorithmProgram
+{
+    public class BubbleSort
+    {
+        public void Bubble()
+        {
+            int[] arr = new int[7] { 1, 5, 7, 8, 6, 5, 9 };
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] <= arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After Sorting Array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0},\n", arr[i]);
+            }
+        }
+    }
+    }
