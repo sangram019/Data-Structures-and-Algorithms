0fbca7ffdd6f48f9bb59753084aebea7e57cadf2
Sangram <sangram15.behera@gmail.com>
2022-05-21 02:13:07 +0530

    Added By Sangram

---
+++ Data-Structures-and-Algorithms/Permutation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Permutation
+{
+	private static void permute(String str,int l, int r)
+	{
+		if (l == r)
+			Console.WriteLine(str);
+		else
+		{
+			for (int i = l; i <= r; i++)
+			{
+				str = swap(str, l, i);
+				permute(str, l + 1, r);
+				str = swap(str, l, i);
+			}
+		}
+	}
+
+	public static String swap(String a,int i, int j)
+	{
+		char temp;
+		char[] charArray = a.ToCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		string s = new string(charArray);
+		return s;
+	}
+
+public static void Main()
+{
+    String str = "ABC";
+    int n = str.Length;
+    permute(str, 0, n - 1);
+}
+}
+
